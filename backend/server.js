56728edd8e67ed83b0388fea3688eeb4fde88c40
cybertechangel <nihad.network@gmail.com>
2025-11-23 18:23:55 +0100

    Modification CSS and DB

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,35 @@
 import express from 'express';
-import dotenv from 'dotenv'; 
+import dotenv from 'dotenv';
 import cors from 'cors';
 import db from './config/db.js';
 
 import articleRoutes from './routes/articleRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
-// Configuration
-dotenv.config(); 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middlewares
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
-// Test de la connexion DB
 db.getConnection()
     .then(connection => {
-        console.log('Connecté à la base de données MySQL !');
-        connection.release(); 
+        console.log('Connected to MySQL database!');
+        connection.release();
     })
     .catch(err => {
-        console.error('Erreur de connexion à la DB:', err.message);
-        process.exit(1); 
+        console.error('Database connection error:', err.message);
+        process.exit(1);
     });
 
-// Routes de l'API
 app.get('/', (req, res) => {
-    res.send('API Blog Fullstack en marche...');
+    res.send('Fullstack Blog API is running...');
 });
 
 app.use('/api/articles', articleRoutes);
 app.use('/api/auth', authRoutes);
 
-// Démarrage du serveur
 app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
